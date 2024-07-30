fde488269c2fc73a981f1acf913a594ed385bc8b
gmeowfilab <labs@gmeow.fi>
2024-07-30 23:02:32 +0700

    remove unused code

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,10 +3,10 @@ import { ethers } from 'hardhat'
 async function main() {
   const [deployer] = await ethers.getSigners()
   const v2Factory = {
-    address: '0xcE9dfc76AA9665865B5138dcc5052dE47981dE6B'
+    address: '0x3874db48bBDD965DA824BE998f63f5cF689D4f79',
   }
   const weth = {
-    address: '0x4200000000000000000000000000000000000006'
+    address: '0x4200000000000000000000000000000000000006',
   }
 
   const gMeowFiV2Router02 = await ethers.deployContract('GMeowFiV2Router02', [v2Factory.address, weth.address])
@@ -18,7 +18,7 @@ async function main() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch(error => {
+main().catch((error) => {
   console.error(error)
   process.exitCode = 1
 })
